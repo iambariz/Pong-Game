39f3655927391f8da5acfd0cf5787a0ca10909bd
iambariz <61651210+iambariz@users.noreply.github.com>
2020-11-25 20:06:46 +0000

    Making the game working
    Just some more update needed

--- app.js
+++ app.js
@@ -46,16 +46,61 @@ function loop(){
     if(p1ToggleUp){ p1Y -= 5;};
     if(p2ToggleDown){ p2Y += 5;};
     if(p2ToggleUp){ p2Y -= 5;};
+    ballX += ballXSpeed; 
+    ballY += ballYSpeed; 
+    
+   
+    if(p1Y < 50){
+        p1Y = 50;
+    }
+    if(p1Y > 550){
+        p1Y = 550;
+    }
+
+    if(p2Y < 50){
+        p2Y = 50;
+    }
+    if(p2Y > 550){
+        p2Y = 550;
+    }
+    
+    
+    
+
+
+    if(ballY < 0 || ballY > 600 ){ ballYSpeed = -ballYSpeed};
+
+    if( ballX < 0 && (ballX > -5 || ballX>=ballXSpeed-5) && Math.abs(ballY - p1Y)<= 55){
+        ballX = 0;
+        ballXSpeed = -ballXSpeed * 1.1;
+        ballYSpeed *= 1.1;
+    }
+
+    if(ballX < -50){ placeBall()};
+
+
+    if( ballX > 700 && (ballX < 705 || ballX - 700 >= ballXSpeed-5)  && Math.abs(ballY - p2Y)<= 55){
+        ballX = 700;
+        ballXSpeed = -ballXSpeed * 1.1;
+        ballYSpeed *= 1.1;
+    }
+
+    if(ballX > 750){ placeBall()};
 
     p1.style.top = p1Y-50+"px";
     p2.style.top = p2Y-50+"px";
+    ball.style.left = ballX-5+50+"px";
+    ball.style.top = ballY-5+"px";
     window.requestAnimationFrame(loop);
 }
 
 loop();
+placeBall();
 
 function placeBall(){
     ballX = 350;
     ballY = 50;
+    ballXSpeed = 4 * (Math.random()*2); 
+    ballYSpeed = 4 * (Math.random()*2);
     
 }
